af642bce1e1e1ebda3b19dbe5c959de3d32662e9
No4ovka <92082996+No4ovka@users.noreply.github.com>
2022-01-26 21:10:32 +0300

    Add files via upload

---
+++ R func.cpp
@@ -0,0 +1,27 @@
+#include "header.h"
+
+void ra(vector <int> &a)
+{
+    int m = a[0], n;
+    for (int k = a.size() - 1; k >= 0; k--)
+    {
+         n = a[k];
+         a[k] = m;
+         m = n;
+    }
+}
+void rb(vector <int> &b)
+{
+    int m = b[0], n;
+    for (int k = b.size() - 1; k >= 0; k--)
+    {
+         n = b[k];
+         b[k] = m;
+         m = n;
+    }
+}
+void rr(vector <int> &a, vector <int> &b)
+{
+    ra(a);
+    rb(b);
+}
